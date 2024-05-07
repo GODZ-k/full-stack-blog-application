85d64d01c73d4e41aeb5699a87e46b79b6741f97
Tanmay/K and A Tech - MERN <kandatech-dev5@APPLEs-MacBook-Pro.local>
2024-05-07 16:40:22 +0530

    routing

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
+  const footer = [
+    {
+      name: "Help",
+      slug: "/help",
+    },
+    {
+      name: "Status",
+      slug: "/help",
+    },
+    {
+      name: "About",
+      slug: "/help",
+    },
+    {
+      name: "Careers",
+      slug: "/help",
+    },{
+      name: "Blog",
+      slug: "/help",
+    },{
+      name: "Privacy",
+      slug: "/help",
+    }
+  ];
   return (
-    <div>Footer</div>
-  )
+    <div className=" border-t border-black flex justify-center items-center py-7">
+      <div className="flex gap-4 text-gray-700">
+        {footer.map((item) => (
+          <div>{item.name}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
-
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 function Header() {
   return (
     <>
-    <div className='w-full h-16 bg-gray-900'>
+      <div className="w-full h-16 flex justify-between items-center px-5 border-b border-black">
         <div>
-        <h5>Logo</h5>
+          <h5>
+            Logo
+          </h5>
         </div>
         <div>
-           <ul className='flex'>
+          <ul className="flex gap-3 cursor-pointer">
+            <li>Write</li>
             <li>Home</li>
-            <li>Add</li>
-           </ul>
+          </ul>
         </div>
         <div>
-<button>Signup</button>
-<button>Login</button>
+          <Link className="px-5 border-gray-900 border mr-3 py-1.5" to="/user/login">
+            Sign in
+          </Link>
+          <Link className="bg-gray-900 text-white px-5 py-2 rounded-sm" to="/user/signup">
+            Get started
+          </Link>
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
